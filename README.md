d4c848455fd99a293766260cd58c6bcaf24a911d
OLEG4120 <oleg02007@gmail.com>
2016-01-22 12:10:45 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MediaInfoCLI-android
 MediaInfo is a convenient unified display of the most relevant technical and tag data for video and audio files. CLI version. Android mod
+<br>
+Prebuild binary files
---
+++ jni/Application.mk
@@ -0,0 +1,13 @@
+APP_PLATFORM := android-19
+
+APP_ABI := armeabi-v7a x86
+#APP_ABI := x86
+
+#APP_STL := stlport_static
+APP_STL := gnustl_static
+#APP_STL := c++_static
+
+#APP_OPTIM := debug
+
+APP_CFLAGS += -Os -fvisibility=hidden
+APP_CPPFLAGS += -fexceptions -fvisibility=hidden
